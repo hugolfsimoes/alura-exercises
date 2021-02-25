38b30d832c72491b6254c3864a65bf1eb085ab6a
Hugo Simoes <hugolfsimoes@gmail.com>
2021-02-25 11:27:13 -0300

    adicionandoa propriedade rg

--- java-script/javaScript-POO/index.js
+++ java-script/javaScript-POO/index.js
@@ -4,6 +4,7 @@ class Cliente {
     this.cpf;
     this.agencia;
     this.saldo;
+    this.rg;
   }
 }
 
@@ -14,6 +15,7 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
+cliente1.rg = 123456789;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
