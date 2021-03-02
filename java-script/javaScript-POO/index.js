34989105c7c2c6ae90864a6b3c8515290e99a680
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-02 09:54:22 -0300

    melhorando a legibilidade do codigo

--- java-script/javaScript-POO/index.js
+++ java-script/javaScript-POO/index.js
@@ -12,18 +12,19 @@ class ContaCorrente {
   sacar(valor) {
     if (this._saldo >= valor) {
       this._saldo -= valor;
+      return valor;
     }
 
   }
 
   depositar(valor) {
-    if (valor > 0) {
-      this._saldo += valor;
-      console.log(this._saldo);
+    if (valor <= 0) {
+      return;
     }
+    this._saldo += valor;
   }
-
 }
+
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -38,7 +39,11 @@ contaCorrenteRicardo.agencia = 1001;
 
 
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
