d489366e4ce01c663478de9112091db7d7238aac
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-08 11:42:47 -0300

    adicionando o construtor com um contador de contas, finalizado o curso.

--- java-script/javaScript-POO/ContaCorrente.js
+++ java-script/javaScript-POO/ContaCorrente.js
@@ -3,8 +3,11 @@ import {
 } from "./Cliente.js";
 
 export class ContaCorrente {
+  static numeroDeContas = 0;
   agencia;
   _cliente;
+
+  _saldo = 0;
 
   set cliente(novoValor) {
 
@@ -15,11 +18,14 @@ export class ContaCorrente {
   get cliente() {
     return this._cliente;
   }
-
-  _saldo = 0;
-
   get saldo() {
     return this._saldo;
+  }
+
+  constructor(cliente, agencia) {
+    this.agencia = agencia;
+    this.cliente = cliente;
+    ContaCorrente.numeroDeContas += 1;
   }
 
 
