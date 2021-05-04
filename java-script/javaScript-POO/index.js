4842c843045d10963af909b5504ac5972a54b043
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-04 16:53:29 -0300

    trabalhando com erros

--- java-script/javaScript-POO/index.js
+++ java-script/javaScript-POO/index.js
@@ -1,4 +1,6 @@
+// Classe Abstrata
 import { Cliente } from "./Cliente.js"
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
@@ -7,11 +9,10 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const conta = new Conta(0, cliente1, 1001);
 
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
+console.log(conta);
