404c743cb05b5e9b57474eb6c41c1bac678f662c
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-04 17:15:33 -0300

    trabalhando com classes e metoros abstratos

--- java-script/javaScript-POO/Conta.js
+++ java-script/javaScript-POO/Conta.js
@@ -1,7 +1,7 @@
 export class Conta {
   constructor (saldoInicial, cliente, agencia) {
     if(this.constructor == Conta) {
-      throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente");
+      throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstrata");
     }
 
     this._saldo = saldoInicial;
@@ -23,10 +23,9 @@ export class Conta {
     return this._saldo;
   }
 
-
+  //Método abstrato
   sacar(valor) {
-    let taxa = 1;
-    return this._sacar(valor, taxa);
+    throw new Error("O método sacar da conta é abstrato");
   }
 
   _sacar(valor, taxa){
--- java-script/javaScript-POO/index.js
+++ java-script/javaScript-POO/index.js
@@ -1,8 +1,8 @@
 // Classe Abstrata
 import { Cliente } from "./Cliente.js"
-import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
@@ -10,9 +10,8 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const conta = new Conta(0, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(100);
 
-
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
-console.log(conta);
+console.log(contaSalario);
