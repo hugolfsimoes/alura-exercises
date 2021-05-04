bade37ba199035f8fef876226ce07468690eb98f
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-04 18:45:53 -0300

    finalizando o curso

--- java-script/javaScript-POO/Funcionarios/Diretor.js
+++ java-script/javaScript-POO/Funcionarios/Diretor.js
@@ -1,7 +1,9 @@
-import { Funcionario } from "./Funcionario.js";
+import {
+  Funcionario
+} from "./Funcionario.js";
 
-export class Diretor extends Funcionario{
-  constructor(nome, salario, cpf){
+export class Diretor extends Funcionario {
+  constructor(nome, salario, cpf) {
     super(nome, salario, cpf);
     this.bonificacao = 2
   }
--- java-script/javaScript-POO/index.js
+++ java-script/javaScript-POO/index.js
@@ -1,8 +1,15 @@
-
-import { Cliente } from "./Cliente.js"
-import { Gerente } from "./Funcionarios/Gerente.js"
-import { Diretor } from "./Funcionarios/Diretor.js"
-import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
+import {
+  Cliente
+} from "./Cliente.js"
+import {
+  Gerente
+} from "./Funcionarios/Gerente.js"
+import {
+  Diretor
+} from "./Funcionarios/Diretor.js"
+import {
+  SistemaAutenticacao
+} from "./SistemaAutenticacao.js"
 
 
 const diretor = new Diretor('Rodrigo', 10000, 12345678900)
